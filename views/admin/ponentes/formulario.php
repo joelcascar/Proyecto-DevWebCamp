a06c7e4235fade7890f2cdb28945d6dc84694a86
joelcascar <joel.adcascar@gmail.com>
2026-07-15 18:49:36 -0600

    formulario de registro de ponente finalizado

---
+++ views/admin/ponentes/formulario.php
@@ -0,0 +1,94 @@
+<fieldset class="formulario__fieldset">
+    <legend class="formulario__legend">Información Personal</legend>
+    <div class="formulario__campo">
+        <label class="formulario__label" for="nombre">Nombre</label>
+        <input type="text" class="formulario__input" id="nombre" name="nombre" placeholder="Nombre ponente" value="<?php echo $ponente->nombre ?? ''; ?>">
+    </div>
+
+    <div class="formulario__campo">
+        <label class="formulario__label" for="apellido">Apellido</label>
+        <input type="text" class="formulario__input" id="apellido" name="apellido" placeholder="Apellido ponente" value="<?php echo $ponente->apellido ?? ''; ?>">
+    </div>
+
+    <div class="formulario__campo">
+        <label class="formulario__label" for="ciudad">Ciudad</label>
+        <input type="text" class="formulario__input" id="ciudad" name="ciudad" placeholder="Ciudad del ponente" value="<?php echo $ponente->ciudad ?? ''; ?>">
+    </div>
+
+    <div class="formulario__campo">
+        <label class="formulario__label" for="pais">País</label>
+        <input type="text" class="formulario__input" id="pais" name="pais" placeholder="País del ponente" value="<?php echo $ponente->pais ?? ''; ?>">
+    </div>
+
+    <div class="formulario__campo">
+        <label class="formulario__label" for="imagen">Imagen</label>
+        <input type="file" class="formulario__input formulario__input--file" id="imagen" name="imagen">
+    </div>
+</fieldset>
+
+<fieldset class="formulario__fieldset">
+    <legend class="formulario__legend">Información Personal</legend>
+    <div class="formulario__campo">
+        <label class="formulario__label" for="tags_input">Áreas de experiencia (separadas por coma).</label>
+        <input type="text" class="formulario__input" id="tags_input" placeholder="Ej. Node.js, PHP, CSS, Lavaravel, UX / UI">
+        <div id="tags" class="formulario__listado"></div>
+        <input type="hidden" name="tags" value="<?php echo $ponente->tags ?? ''; ?>">
+    </div>
+</fieldset>
+
+<fieldset class="formulario__fieldset">
+    <legend class="formulario__legend">Redes sociales</legend>
+    <div class="formulario__campo">
+        <div class="formulario__contenedor-icono">
+            <div class="formulario__icono">
+                <i class="fa-brands fa-facebook"></i>
+            </div>
+            <input type="text" class="formulario__input--sociales" name="redes[facebook]" placeholder="Facebook" value="<?php echo $ponente->facebook ?? ''; ?>">
+        </div>
+    </div>
+
+    <div class="formulario__campo">
+        <div class="formulario__contenedor-icono">
+            <div class="formulario__icono">
+                <i class="fa-brands fa-x-twitter"></i>
+            </div>
+            <input type="text" class="formulario__input--sociales" name="redes[twitter]" placeholder="X (antes twitter)" value="<?php echo $ponente->twiiter ?? ''; ?>">
+        </div>
+    </div>
+
+    <div class="formulario__campo">
+        <div class="formulario__contenedor-icono">
+            <div class="formulario__icono">
+                <i class="fa-brands fa-youtube"></i>
+            </div>
+            <input type="text" class="formulario__input--sociales" name="redes[youtube]" placeholder="Youtube" value="<?php echo $ponente->youtube ?? ''; ?>">
+        </div>
+    </div>
+
+    <div class="formulario__campo">
+        <div class="formulario__contenedor-icono">
+            <div class="formulario__icono">
+                <i class="fa-brands fa-instagram"></i>
+            </div>
+            <input type="text" class="formulario__input--sociales" name="redes[instagram]" placeholder="Instagram" value="<?php echo $ponente->youtube ?? ''; ?>">
+        </div>
+    </div>
+
+    <div class="formulario__campo">
+        <div class="formulario__contenedor-icono">
+            <div class="formulario__icono">
+                <i class="fa-brands fa-tiktok"></i>
+            </div>
+            <input type="text" class="formulario__input--sociales" name="redes[tiktok]" placeholder="Tiktok" value="<?php echo $ponente->tiktok ?? ''; ?>">
+        </div>
+    </div>
+
+    <div class="formulario__campo">
+        <div class="formulario__contenedor-icono">
+            <div class="formulario__icono">
+                <i class="fa-brands fa-github"></i>
+            </div>
+            <input type="text" class="formulario__input--sociales" name="redes[github]" placeholder="Github" value="<?php echo $ponente->github ?? ''; ?>">
+        </div>
+    </div>
+</fieldset>
