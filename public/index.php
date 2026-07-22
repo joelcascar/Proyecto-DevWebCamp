b81ae1e087964909158b6409e2ef6fcd4d5d3d88
joelcascar <joel.adcascar@gmail.com>
2026-07-22 01:08:33 -0600

    listar, actualizar y eliminar eventos finalizado

--- public/index.php
+++ public/index.php
@@ -49,12 +49,17 @@ $router->post('/admin/ponentes/eliminar', [PonentesController::class, 'eliminar'
 $router->get('/admin/eventos', [EventosController::class, 'index']);
 $router->get('/admin/eventos/crear', [EventosController::class, 'crear']);
 $router->post('/admin/eventos/crear', [EventosController::class, 'crear']);
+$router->get('/admin/eventos/editar', [EventosController::class, 'editar']);
+$router->post('/admin/eventos/editar', [EventosController::class, 'editar']);
+$router->post('/admin/eventos/eliminar', [EventosController::class, 'eliminar']);
 
 // API 
 $router->get('/api/eventos-horario', [APIEventos::class, 'index']);
 $router->get('/api/ponentes', [APIPonentes::class, 'index']);
+$router->get('/api/ponente', [APIPonentes::class, 'ponente']);
 // Registrados
 $router->get('/admin/registrados', [RegistradosController::class, 'index']);
+// Regalos
 $router->get('/admin/regalos', [RegalosController::class, 'index']);
 
 
